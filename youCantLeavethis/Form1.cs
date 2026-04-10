016641c7f45d05ef281f599705b088c17116ace9
czarekkdev <sys.krnl@proton.me>
2026-04-10 04:09:03 +0200

    Remove unused using directives from Form1.cs
    Cleaned up Form1.cs by removing unnecessary using statements:
    System.Collections.Generic, System.ComponentModel, System.Data,
    System.Data.SqlTypes, and System.Diagnostics. This reduces
    dependencies and improves code readability.

--- youCantLeavethis/Form1.cs
+++ youCantLeavethis/Form1.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlTypes;
-using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace youCantLeavethis
